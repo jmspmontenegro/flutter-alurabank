639c6bc44920bec77dcd5f0df2721a3b46947d47
jmspmontenegro <jmsp.montenegro@gmail.com>
2021-05-28 15:31:57 -0300

    Primeiro Commit

---
+++ android/app/src/main/kotlin/com/example/alurabank/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.alurabank
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Text("Hello world.", textDirection: TextDirection.ltr,));
