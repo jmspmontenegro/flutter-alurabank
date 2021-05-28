fac12e55a52f0464fc59d9c81a94c407709bd38e
jmspmontenegro <jmsp.montenegro@gmail.com>
2021-05-28 17:44:39 -0300

    Primeira refatoração e estado de um componente

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,53 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text("Hello world.", textDirection: TextDirection.ltr,));
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: ListaTransferencia(),
+        appBar: AppBar(
+          title: Text("Alura Bank - Transferências"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            //ao clicar
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
+    ));
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(120.22,9898)),
+        ItemTransferencia(Transferencia(77.00,9898)),
+        ItemTransferencia(Transferencia(1200.00,9898)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia{
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
